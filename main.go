7ed1f02cba688194e9f30ab6ebd3cca99a8ab515
Brian Illes <brianrilles@gmail.com>
2019-04-21 08:23:56 -0700

    Adds logging

--- main.go
+++ main.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Sanity</h1>")
-}
 
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/protected", protected).Methods("GET")
 
 	var port = ":6000"
 	fmt.Println("\n*** API running on http://localhost" + port + " ***\n")
-	log.Fatal(http.ListenAndServe(port, r))
+	log.Fatal(http.ListenAndServe(port, handlers.LoggingHandler(os.Stdout, r)))
+}
+
+var home = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Sanity</h1>")
+}
+
+var protected = func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 }
